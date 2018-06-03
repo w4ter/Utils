1f9a62342f55009e84b6b6899131a87702ae6448
M4xW4n9 <aboultraman@gmail.com>
2018-06-03 18:07:58 +0800

    change the width and height of a bmp

--- bruteBmpWH.py
+++ bruteBmpWH.py
@@ -4,19 +4,16 @@ __Auther__ = 'M4x'
 
 import struct
 
-All = 1024000 / 32
+All = 1024000 / 32 * 8
 
 def saveBmp(name, width, height):
-    with open("./buzzing.bmp") as f:
+    with open("./buzzing.bmp", "rb") as f:
         s = f.read()
 
-    with open(name + ".bmp", "w") as f:
+    with open(name + ".bmp", "wb") as f:
         f.write(s[: 0x12] + struct.pack('<i', width) + struct.pack('<i', height) + s[0x1a: ])
 
 for i in xrange(1, All):
     if All % i == 0:
         #  print i, All / i
         saveBmp(str(i), i, All / i)
-
-#  s[0x1c: 0x1e]
-#  struct.pack('<H', 200)
